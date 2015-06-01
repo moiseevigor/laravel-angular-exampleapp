82f9afb6a3d6651b841f8ac1eae11bcac89bce2e
Igor Moiseev <moiseev.igor@gmail.com>
2015-06-02 00:56:04 +0300

    travis

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+
 class ExampleTest extends TestCase {
 
 	/**
@@ -7,11 +9,23 @@ class ExampleTest extends TestCase {
 	 *
 	 * @return void
 	 */
-	public function testBasicExample()
+	public function testRoot()
 	{
 		$response = $this->call('GET', '/');
 
 		$this->assertEquals(200, $response->getStatusCode());
 	}
 
+	/**
+	 * A basic functional test example.
+	 *
+	 * @return void
+	 */
+	public function testCAP()
+	{
+		$response = $this->call('GET', '/cap/search?query=34100');
+
+    	$this->assertResponseOk();
+		$this->assertEquals('[{"id":"34100","full_name":"34100 - TRIESTE"}]', $response->getContent());
+    }
 }
